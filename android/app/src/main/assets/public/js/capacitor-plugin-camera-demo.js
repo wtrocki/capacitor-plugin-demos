a4c0901bf723be25b52424d8cbb2fbe6b0422cdd
Jan Piotrowski <piotrowski@gmail.com>
2018-03-04 23:44:11 +0100

    npx cap copy

--- android/app/src/main/assets/public/js/capacitor-plugin-camera-demo.js
+++ android/app/src/main/assets/public/js/capacitor-plugin-camera-demo.js
@@ -1,6 +1,3 @@
-
-
-
 window.customElements.define('capacitor-plugin-camera-demo', class extends HTMLElement {
   constructor() {
     super();
@@ -14,20 +11,17 @@ window.customElements.define('capacitor-plugin-camera-demo', class extends HTMLE
         <img id="photo" style="max-width:100%; margin-top:15px;"/>
     </div>
     `
-
     this.addEventListener('click', this.takePicture);
-
   }
 
-async takePicture() {
-    const image = await Capacitor.Plugins.Camera.getPhoto({
-        quality: 90,
-        allowEditing: true,
-        resultType: 'base64'
-    });
-    var imageUrl = image.base64_data;
-    console.log(this.shadowRoot);
-    this.shadowRoot.getElementById("photo").src = imageUrl;
-}
+    async takePicture() {
+        const image = await Capacitor.Plugins.Camera.getPhoto({
+            quality: 90,
+            allowEditing: true,
+            resultType: 'base64'
+        });
+        var imageUrl = image.base64_data;
+        this.shadowRoot.getElementById("photo").src = imageUrl;
+    }
   
 });
